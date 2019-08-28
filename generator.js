fe594bc8d623682c42d937afa4f27de8a5702fe1
nmblunier <nmblunier@gmail.com>
2019-08-27 20:54:29 -0500

    Basic functionality working

--- generator.js
+++ generator.js
@@ -1,3 +1,13 @@
-function printAlert() {
-    window.alert('Hello world!');
+function generateUsername() {
+    adjListLength = onesyllableadjectives.length;
+    nounListLength = nouns.length;
+
+    adjRandomIndex = Math.floor(Math.random() * adjListLength);
+    nounRandomIndex = Math.floor(Math.random() * nounListLength);
+
+    document.getElementById("username").innerHTML = (onesyllableadjectives[adjRandomIndex] + capitalizeFirstLetter(nouns[nounRandomIndex]));
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
